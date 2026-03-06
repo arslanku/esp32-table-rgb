a9ff90f0f03f299820f1b19b50725017634a7306
arslan <@ItMan07>
2026-03-06 17:31:44 +0300

    upd settings

--- src/settings.h
+++ src/settings.h
@@ -3,6 +3,7 @@
 
 #define WIFI_SSID "CHAIHANA_HOUSE"
 #define WIFI_PASSWORD "987123654"
+
 #define MQTT_SERVER "mqtt.dealgate.ru"
 #define MQTT_PORT 1883
 #define MQTT_USER "itman7144"
@@ -16,12 +17,8 @@
 #include <PubSubClient.h>
 #include <FastLED.h>
 
-// =====================
 #define LED_BUILTIN 2
 #define LOGGING 1
-
-// =====================
-// Настройки WiFi и MQTT
 
 #define MQTT_CLIENT_NAME "ESP32-client-" // + ...
 #define TOPIC_TO_PUBLISH "STRIP-table-1-recieved"
@@ -30,9 +27,6 @@
 #define STRIP_MODE_TOPIC "STRIP-table-mode"
 #define STRIP_SPEED "STRIP-table-speed"
 #define RESTART_OTA_TOPIC "STRIP-table-1-OTA"
-
-// =====================
-// Настройки FastLED
 
 #define NUM_LEDS 120
 #define DATA_PIN 5
@@ -49,12 +43,8 @@
 #include <PubSubClient.h>
 #include <FastLED.h>
 
-// =====================
 #define LED_BUILTIN D4
 #define LOGGING 1
-
-// =====================
-// Настройки WiFi и MQTT
 
 #define MQTT_CLIENT_NAME "ESP8266-client-" // + ...
 #define TOPIC_TO_PUBLISH "STRIP-table-2-recieved"
@@ -62,9 +52,6 @@
 #define STRIP_BRIGHTNESS_TOPIC "STRIP-table-brightness"
 #define STRIP_MODE_TOPIC "STRIP-table-mode"
 #define RESTART_OTA_TOPIC "STRIP-table-2-OTA"
-
-// =====================
-// Настройки FastLED
 
 #define NUM_LEDS 120
 #define DATA_PIN D1
@@ -81,12 +68,8 @@
 #include <PubSubClient.h>
 #include <FastLED.h>
 
-// =====================
 #define LED_BUILTIN D4
 #define LOGGING 1
-
-// =====================
-// Настройки WiFi и MQTT
 
 #define MQTT_CLIENT_NAME "ESP8266-client-" // + ...
 #define TOPIC_TO_PUBLISH "STRIP-window-recieved"
@@ -94,9 +77,6 @@
 #define STRIP_BRIGHTNESS_TOPIC "STRIP-window-brightness"
 #define STRIP_MODE_TOPIC "STRIP-window-mode"
 #define RESTART_OTA_TOPIC "STRIP-window-OTA"
-
-// =====================
-// Настройки FastLED
 
 #define NUM_LEDS 380
 #define DATA_PIN D1
